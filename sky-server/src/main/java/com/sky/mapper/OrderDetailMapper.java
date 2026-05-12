8356b76ce87b4eceea6801f875ae5d421eaa978b
alex <962213127@qq.com>
2026-05-12 10:53:55 +0800

    add admin-orders-management module

--- sky-server/src/main/java/com/sky/mapper/OrderDetailMapper.java
+++ sky-server/src/main/java/com/sky/mapper/OrderDetailMapper.java
@@ -6,4 +6,12 @@ import java.util.List;
 
 public interface OrderDetailMapper {
     void insertBatch(List<OrderDetail> orderDetailList);
+
+
+    /**
+     * 查询订单明细
+     * @param ordersId
+     * @return
+     */
+    List<OrderDetail> getByOrderId(Long ordersId);
 }
--- sky-server/src/main/java/com/sky/mapper/ShoppingCartMapper.java
+++ sky-server/src/main/java/com/sky/mapper/ShoppingCartMapper.java
@@ -16,4 +16,11 @@ public interface ShoppingCartMapper {
     void deleteByUserId(Long currentId);
 
     void deleteById(Long id);
+
+
+    /**
+     * 将购物车对象批量添加到购物车
+     * @param shoppingCartList
+     */
+    void insertBatch(List<ShoppingCart> shoppingCartList);
 }
